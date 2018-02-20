88f81af1adf13386b880cdd8a3f2cf3f48374bec
Clement Barbaza <clement.barbaza@gmail.com>
2018-02-21 00:24:50 +0100

    libs

---
+++ src/Csrf/Csrf.php
@@ -0,0 +1,18 @@
+<?php
+namespace Icarus;
+
+/**
+ * CSRF protection
+ */
+class Csrf {
+
+    /**
+     * Load CSRF Protection
+     *
+     * @return void
+     */
+    public function invoke() {
+        return new \Slim\Csrf\Guard;
+    }
+
+}
